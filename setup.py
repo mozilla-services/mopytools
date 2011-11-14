f563d10d9b3d38dd0bf9f293d121d8d060a88b43
Tarek Ziade <tarek@ziade.org>
2011-11-14 14:34:55 -0800

    startng 1.8

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ package_data = {
                             "services_base/+package+/tests/*.*"]}
 
 
-setup(name='MoPyTools', version='1.7',
+setup(name='MoPyTools', version='1.8',
       author="Tarek Ziade", author_email="tarek@mozilla.com",
       url="http://hg.mozilla.org/services/server-devtools",
       description="Set of tools to build Mozilla Services apps",
