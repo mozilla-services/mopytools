cc43e4c215599a94d63d07004f94319b43ae1045
Rob Miller <rob@kalistra.com>
2012-01-25 14:10:59 -0800

    post 2.4 release version bumps

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ package_data = {
                             "services_base/+package+/tests/*.*"]}
 
 
-setup(name='MoPyTools', version='2.4',
+setup(name='MoPyTools', version='2.5',
       author="Tarek Ziade", author_email="tarek@mozilla.com",
       url="http://hg.mozilla.org/services/server-devtools",
       description="Set of tools to build Mozilla Services apps",
